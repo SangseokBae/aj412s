856bfad8c80f1a3f857a731c6d66e77b3625f6b7
SangseokBae <[ssb9018@ajou.ac.kr]>
2023-03-24 09:51:21 +0900

    first commit

--- R/c2n.R
+++ R/c2n.R
@@ -1,4 +1,6 @@
 # 문자열을 numeric으로  
 c2n <- function(x){   
      groups = unique(x)   
-     as.numeric(factor(x, levels=groups)) }   
+     tmp<-as.numeric(factor(x, levels=groups))
+	 tmp<-as.data.frame(tmp)
+}
